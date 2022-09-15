ed7ac254a3ae549b192bfb63526322571ae1c9f9
William Soares <williamsoares058@gmail.com>
2022-09-15 00:40:48 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
