7cce37e738a3afd247992a9e2c0ee9e15fc9dac7
William Soares <williamsoares058@gmail.com>
2022-09-15 00:57:30 -0300

    POST Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -27,8 +27,17 @@ public class CategoryService {
 
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
-       Optional<Category> obj = repository.findById(id);
+        Optional<Category> obj = repository.findById(id);
         Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
+
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO categoryDTO) {
+        Category entity = new Category();
+        entity.setName(categoryDTO.getName());
+        entity = repository.save(entity);
         return new CategoryDTO(entity);
     }
 }
